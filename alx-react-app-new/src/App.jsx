b1c3ece2722a4fc57a614ea4a6429dadbb2f862a
samuel wagura <samwagura@gmail.com>
2025-11-14 18:37:54 +0300

    Move Counter.jsx to src and update App.jsx import

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-import Counter from './components/Counter';
+import Counter from './Counter';
 
 function App() {
   return (
